2af43cd4bdc18d86baeb9c738d2a9086da7d4e24
Itamar Schwartz <itamarschwartz10@gmail.com>
2024-07-01 16:23:51 +0300

    Seperate to encrypt and dont encrypt messages

--- src/network_handler.rs
+++ src/network_handler.rs
@@ -32,26 +32,36 @@ where
         })
     }
 
-    pub async fn write_buf_encrypt(&mut self, buf: &[u8]) -> anyhow::Result<()> {
-        let encrypted_message = self.encryptor.encrypt(buf);
-
-        let len_bytes = (encrypted_message.len() as u32).to_le_bytes();
-        let mut buf = Vec::with_capacity(len_bytes.len() + encrypted_message.len());
+    pub async fn write(&mut self, message: &[u8]) -> anyhow::Result<()> {
+        let len_bytes = (message.len() as u32).to_le_bytes();
+        let mut buf = Vec::with_capacity(len_bytes.len() + message.len());
 
         buf.extend_from_slice(&len_bytes);
-        buf.extend_from_slice(&encrypted_message);
+        buf.extend_from_slice(message);
 
         self.writer.write_all(&buf).await?;
         Ok(())
     }
 
-    pub async fn read_buf_decrypt(&mut self) -> anyhow::Result<Vec<u8>> {
+    pub async fn read(&mut self) -> anyhow::Result<Vec<u8>> {
         let len = self.reader.read_u32_le().await? as usize;
 
         let mut buf = vec![0; len];
         self.reader.read_exact(&mut buf[..]).await?;
+        Ok(buf)
+    }
 
+    pub async fn write_buf_encrypt(&mut self, buf: &[u8]) -> anyhow::Result<()> {
+        let encrypted_message = self.encryptor.encrypt(buf);
+        self.write(&encrypted_message).await?;
+
+        Ok(())
+    }
+
+    pub async fn read_buf_decrypt(&mut self) -> anyhow::Result<Vec<u8>> {
+        let buf = self.read().await?;
         let message = self.encryptor.decrypt(&buf)?;
+
         Ok(message)
     }
 }
