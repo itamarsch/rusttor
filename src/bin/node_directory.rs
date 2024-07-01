9fc526554ba9c1a2e5f3735b56700c6e895b139b
Itamar Schwartz <itamarschwartz10@gmail.com>
2024-07-01 14:16:51 +0300

    Node directory http server

---
+++ src/bin/node_directory.rs
@@ -0,0 +1,45 @@
+use std::{collections::BTreeSet, net::Ipv4Addr};
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use const_format::concatcp;
+use serde::{Deserialize, Serialize};
+use tokio::sync::Mutex;
+
+const PORT: u16 = 30000;
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, PartialOrd, Ord, Eq)]
+struct Node {
+    ip: Ipv4Addr,
+    port: u16,
+}
+
+#[derive(Default)]
+struct AppState {
+    nodes: Mutex<BTreeSet<Node>>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let data = web::Data::new(AppState::default());
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(data.clone())
+            .route("/add_node", web::post().to(add_node))
+            .route("/get_nodes", web::get().to(get_nodes))
+    })
+    .bind(concatcp!("0.0.0.0:", PORT))?
+    .run()
+    .await
+}
+
+async fn add_node(data: web::Data<AppState>, node: web::Json<Node>) -> impl Responder {
+    let mut nodes = data.nodes.lock().await;
+    nodes.insert(node.into_inner());
+    HttpResponse::Ok().body("Node added")
+}
+
+async fn get_nodes(data: web::Data<AppState>) -> impl Responder {
+    let nodes = data.nodes.lock().await;
+    HttpResponse::Ok().json(&*nodes)
+}
