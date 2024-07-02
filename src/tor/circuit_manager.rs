00733faff38556fe5c640e875e98e95caff208df
Itamar Schwartz <itamarschwartz10@gmail.com>
2024-07-02 22:34:34 +0300

    Reorganize circuit manager code

--- src/tor/circuit_manager.rs
+++ src/tor/circuit_manager.rs
@@ -22,47 +22,53 @@ pub struct CircuitManager {
 }
 
 impl CircuitManager {
-    pub fn on_message(&mut self, message: RequestMessage) -> anyhow::Result<ResultMessage> {
+    pub fn message(&mut self, message: RequestMessage) -> anyhow::Result<ResultMessage> {
         match message {
-            Directional::Forward(TorMessage::NotForYou { data }) => {
-                let Some(ref encryptor) = &self.encryptor else {
-                    anyhow::bail!("received notforyou before handshake")
-                };
-
-                let deonionized = encryptor.decrypt(&data[..])?;
-                let next_message: MoveAlongMessage = bincode::deserialize(&deonionized[..])?;
-
-                Ok(Directional::Forward(next_message))
-            }
-
-            Directional::Forward(TorMessage::HandShake(other_public_key)) => {
-                if self.encryptor.is_some() {
-                    anyhow::bail!("Received handshake after handshake complete")
-                }
-
-                let (encryptor, my_public) = Encryptor::from_public(other_public_key);
-
-                self.encryptor = Some(encryptor);
-
-                Ok(Directional::Back(TorMessage::HandShake(my_public)))
-            }
-            Directional::Back(message) => {
-                let Some(ref encryptor) = &self.encryptor else {
-                    anyhow::bail!("received notforyou before handshake")
-                };
-
-                let data = bincode::serialize(&message)?;
-
-                let encrypted_back = encryptor.encrypt(&data[..]);
-
-                Ok(Directional::Back(TorMessage::NotForYou {
-                    data: encrypted_back,
-                }))
-            }
+            Directional::Forward(TorMessage::HandShake(public_key)) => self.handshake(public_key),
+            Directional::Forward(TorMessage::NotForYou { data }) => self.push_onward(data),
+            Directional::Back(message) => self.push_response_back(message),
         }
     }
 
-    // Used for test purposes only!
+    fn handshake(&mut self, other_public_key: [u8; 32]) -> anyhow::Result<ResultMessage> {
+        if self.encryptor.is_some() {
+            anyhow::bail!("Received handshake after handshake complete")
+        }
+
+        let (encryptor, my_public) = Encryptor::from_public(other_public_key);
+
+        self.encryptor = Some(encryptor);
+
+        Ok(Directional::Back(TorMessage::HandShake(my_public)))
+    }
+
+    pub fn push_onward(&mut self, onioned_data: Vec<u8>) -> anyhow::Result<ResultMessage> {
+        let Some(ref encryptor) = &self.encryptor else {
+            anyhow::bail!("received notforyou before handshake")
+        };
+
+        let deonionized = encryptor.decrypt(&onioned_data[..])?;
+        let next_message: MoveAlongMessage = bincode::deserialize(&deonionized[..])?;
+
+        Ok(Directional::Forward(next_message))
+    }
+
+    fn push_response_back<T>(&mut self, message: T) -> anyhow::Result<ResultMessage>
+    where
+        T: Serialize,
+    {
+        let Some(ref encryptor) = &self.encryptor else {
+            anyhow::bail!("received notforyou before handshake")
+        };
+
+        let data = bincode::serialize(&message)?;
+
+        let encrypted_back = encryptor.encrypt(&data[..]);
+
+        Ok(Directional::Back(TorMessage::NotForYou {
+            data: encrypted_back,
+        }))
+    }
 }
 
 #[cfg(test)]
@@ -113,7 +119,7 @@ mod tests {
             data: bob.encrypt(&bincode::serialize(&move_along)?[..]),
         };
 
-        let result = circuit_manager.on_message(super::Directional::Forward(message))?;
+        let result = circuit_manager.message(super::Directional::Forward(message))?;
 
         assert_eq!(result, Directional::Forward(move_along));
         Ok(())
@@ -128,7 +134,7 @@ mod tests {
         let handshake = TorMessage::HandShake(bob.initial_public_message());
 
         let Directional::Back(TorMessage::HandShake(pubkey)) =
-            circuit_manager.on_message(Directional::Forward(handshake))?
+            circuit_manager.message(Directional::Forward(handshake))?
         else {
             panic!("Handshake response wasn't sent back")
         };
@@ -159,7 +165,7 @@ mod tests {
         let data = vec![1, 2, 3];
         let Directional::Back(TorMessage::NotForYou {
             data: encrypted_data,
-        }) = circuit_manager.on_message(Directional::Back(TorMessage::NotForYou {
+        }) = circuit_manager.message(Directional::Back(TorMessage::NotForYou {
             data: data.clone(),
         }))?
         else {
